daae5af790ec849211101b55c042ad09f80ca57e
rusetiq <stancedmediaae@gmail.com>
2025-12-19 18:36:23 +0400

    possibly fix vercel deployment

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ initDatabase();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://nafira.vercel.app"
 ];
 
 
